0d295f59b85673b8cf3ddfae2bec642c4f35051a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-01-31 05:06:23 -0500

    spelling: referent
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- test/unit/org/apache/cassandra/db/memtable/MemtableSizeTestBase.java
+++ test/unit/org/apache/cassandra/db/memtable/MemtableSizeTestBase.java
@@ -187,7 +187,7 @@ public abstract class MemtableSizeTestBase extends CQLTester
             {
                 // Under Java 11, it seems the meter can reach into phantom reference queues and count more space than
                 // is actually reachable. Unfortunately ignoreNonStrongReferences() does not help (worse, it throws
-                // exceptions trying to get a phantom referrent). Retrying the measurement appears to clear these up.
+                // exceptions trying to get a phantom referent). Retrying the measurement appears to clear these up.
                 Thread.sleep(50);
                 long secondPass = meter.measureDeep(memtable);
                 logger.error("Deep size first pass {} second pass {}",
